f29feda0ee83768f6bd7e06f01ea4df36e660c01
John Zhuang <a-jzhuang@expedia.com>
2018-07-09 15:42:13 -0700

    more database impl

--- src/main/java/com/expedia/database/DataAccessLayerImpl.java
+++ src/main/java/com/expedia/database/DataAccessLayerImpl.java
@@ -1,15 +1,24 @@
 package com.expedia.database;
 
+import com.github.fakemongo.Fongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
 import javax.inject.Named;
 import java.util.List;
 
 @Named
 public class DataAccessLayerImpl implements DataAccessLayer {
-	@Override public void signUp(String username, String password) {
+	private Fongo fongo = new Fongo("mongo server 1");
+	private DB db = fongo.getDB("mydb");
+	private DBCollection collection = db.getCollection("mycollection");
 
+	@Override
+	public void signUp(String username, String password) {
+//		collection.insert(new BasicDBObject("name", "jon"));
 	}
 
-	@Override public void populateMetaData(List<Object> airlines) {
-
+	@Override
+	public void populateMetaData(List<Object> airlines) {
 	}
 }
