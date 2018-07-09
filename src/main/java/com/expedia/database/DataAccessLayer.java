ee5dd7fd6435b2e48489053356f683cebe2feaf6
John Zhuang <a-jzhuang@expedia.com>
2018-07-09 14:52:54 -0700

    comments

--- src/main/java/com/expedia/database/DataAccessLayer.java
+++ src/main/java/com/expedia/database/DataAccessLayer.java
@@ -1,8 +1,13 @@
 package com.expedia.database;
 
+import java.util.List;
+
 /**
  * Populate this interface as needed.
  */
 public interface DataAccessLayer {
 	void signUp(String username, String password);
+
+	// TODO Replace 'Object' with Airline/Hotel/Account, etc
+	void populateMetaData(List<Object> airlines);
 }
